09c13143d185c31a06004b52ca686e6a3795ac76
Bruno Fialho <brugerf@gmail.com>
2024-01-05 19:13:50 -0300

    Company controller

--- src/main/java/com/ignite/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/ignite/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,11 +1,12 @@
 package com.ignite.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import com.ignite.gestao_vagas.exceptions.UserFoundException;
 import com.ignite.gestao_vagas.modules.company.entities.CompanyEntity;
 import com.ignite.gestao_vagas.modules.company.repositories.CompanyRepository;
 
+@Service
 public class CreateCompanyUseCase {
   
   @Autowired
