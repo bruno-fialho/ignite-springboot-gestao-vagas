8ad8d7ffff152d17d1c5c87fb07d6ecd1a1c6f16
Bruno Fialho <brugerf@gmail.com>
2024-01-09 20:38:20 -0300

    ProfileCandidate use case

--- src/main/java/com/ignite/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/ignite/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,22 +1,29 @@
 package com.ignite.gestao_vagas.modules.candidate.controllers;
 
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import com.ignite.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import com.ignite.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import com.ignite.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
-
+  
   @Autowired
   private CreateCandidateUseCase createCandidateUseCase;
+  
+  @Autowired
+  private ProfileCandidateUseCase profileCandidateUseCase;
   
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -27,5 +34,17 @@ public class CandidateController {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
   }
-
+  
+  @GetMapping("/")
+  public ResponseEntity<Object> get(HttpServletRequest request) {
+    var idCandidate = request.getAttribute("candidate_id");
+    
+    try {
+      var profile = this.profileCandidateUseCase
+        .execute(UUID.fromString(idCandidate.toString()));
+      return ResponseEntity.ok().body(profile);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
 }
