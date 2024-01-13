0e649c7db3198c691b7e1f83f62099d7c91f9879
Bruno Fialho <brugerf@gmail.com>
2024-01-13 15:43:41 -0300

    Security candidate filter and pre authorize

--- src/main/java/com/ignite/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/ignite/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ignite.gestao_vagas.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,10 +11,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
   
   @Autowired
   private SecurityFilter securityFilter;
+
+  @Autowired
+  private SecurityCandidateFilter securityCandidateFilter;
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,7 +31,8 @@ public class SecurityConfig {
           .requestMatchers("/candidate/auth").permitAll();
         auth.anyRequest().authenticated();
       })
-      .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+      .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+      .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class);
 
     return http.build();
   }
