dedbd6828e2d0b5f40bfc70102a463876e6f7a20
Bruno Fialho <brugerf@gmail.com>
2024-03-26 15:42:46 -0300

    Config Swagger on CandidateController

--- src/main/java/com/ignite/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/ignite/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -19,6 +19,13 @@ import com.ignite.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase
 import com.ignite.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import com.ignite.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 import com.ignite.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/candidate")
@@ -58,6 +65,11 @@ public class CandidateController {
 
   @GetMapping("/job")
   @PreAuthorize("hasRole('CANDIDATE')")
+  @Tag(name = "Candidato", description = "Informações do candidato")
+  @Operation(summary = "Listagem de vagas disponíveis para o candidato",
+      description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro")
+  @ApiResponses(@ApiResponse(responseCode = "200", content = {
+      @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))}))
   public List<JobEntity> findJobByFilter(@RequestParam String filter) {
     return this.listAllJobsByFilterUseCase.execute(filter);
   }
