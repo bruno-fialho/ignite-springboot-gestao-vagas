ed1b75db3857f43ec16bc5a9abb7e6b342f4280d
Bruno Fialho <brugerf@gmail.com>
2024-03-26 15:35:34 -0300

    ListAllJobsByFilterUseCase

--- src/main/java/com/ignite/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/ignite/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package com.ignite.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,24 +9,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import com.ignite.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import com.ignite.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import com.ignite.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import com.ignite.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import com.ignite.gestao_vagas.modules.company.entities.JobEntity;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
-  
+
   @Autowired
   private CreateCandidateUseCase createCandidateUseCase;
-  
+
   @Autowired
   private ProfileCandidateUseCase profileCandidateUseCase;
-  
+
+  @Autowired
+  private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
+
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
     try {
@@ -35,18 +42,23 @@ public class CandidateController {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
   }
-  
+
   @GetMapping("/")
   @PreAuthorize("hasRole('CANDIDATE')")
   public ResponseEntity<Object> get(HttpServletRequest request) {
     var idCandidate = request.getAttribute("candidate_id");
-    
+
     try {
-      var profile = this.profileCandidateUseCase
-        .execute(UUID.fromString(idCandidate.toString()));
+      var profile = this.profileCandidateUseCase.execute(UUID.fromString(idCandidate.toString()));
       return ResponseEntity.ok().body(profile);
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
+  }
+
+  @GetMapping("/job")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+    return this.listAllJobsByFilterUseCase.execute(filter);
   }
 }
