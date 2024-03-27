cb397001d1cf85bc1e0791a340eb228330d10356
Bruno Fialho <brugerf@gmail.com>
2024-03-27 19:45:33 -0300

    Job Swagger config

--- src/main/java/com/ignite/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/ignite/gestao_vagas/modules/company/controllers/JobController.java
@@ -12,27 +12,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ignite.gestao_vagas.modules.company.dto.CreateJobDTO;
 import com.ignite.gestao_vagas.modules.company.entities.JobEntity;
 import com.ignite.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/company/job")
 public class JobController {
-  
+
   @Autowired
   private CreateJobUseCase createJobUseCase;
 
   @PostMapping("/")
   @PreAuthorize("hasRole('COMPANY')")
-  public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+  @Tag(name = "Vagas", description = "Informações das vagas")
+  @Operation(summary = "Cadastro de vaga",
+      description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+  @ApiResponses(@ApiResponse(responseCode = "200",
+      content = {@Content(schema = @Schema(implementation = JobEntity.class))}))
+  @SecurityRequirement(name = "jwt_auth")
+  public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO,
+      HttpServletRequest request) {
     var companyId = request.getAttribute("company_id");
 
-    var jobEntity = JobEntity.builder()
-        .benefits(createJobDTO.getBenefits())
-        .companyId(UUID.fromString(companyId.toString()))
-        .description(createJobDTO.getDescription())
-        .level(createJobDTO.getLevel())
-        .build();
+    var jobEntity = JobEntity.builder().benefits(createJobDTO.getBenefits())
+        .companyId(UUID.fromString(companyId.toString())).description(createJobDTO.getDescription())
+        .level(createJobDTO.getLevel()).build();
 
     return createJobUseCase.execute(jobEntity);
   }
